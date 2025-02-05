08e5d930eec45e73f4cc48dd2b68f31a7d171c31
s-bye <kill46923@gmail.com>
2025-02-05 14:51:40 +0600

    Added tests

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from classes.Amount import Amount
 from classes.Personal_Account import PersonalAccount
 
 if __name__ == "__main__":
-    amount = Amount(1000, "Deposit")
-    print(amount.amount)
-    print(amount.transaction_type)
+    account = PersonalAccount(123456, "Abai Nurlanov", 0, [])
+    print(account)
+    account.deposit(Amount(100, "Deposit"))
+    account.withdraw(Amount(50, "Withdraw"))
+    account.transaction_history()
+    print(account.get_balance())
+    print(account.get_account_number())
+    print(account.get_account_holder())
+    account.set_account_number(654321)
+    account.set_account_holder("Baiastan Zamirbekov")
+    print(account)
+    account + Amount(200, "Deposit")
+    account - Amount(100, "Withdraw")
+    print(account)
